c25b2f07959410058dca521d1ec61d60645e4052
Rick Potter <36988302+RickHPotter@users.noreply.github.com>
2021-08-18 18:03:47 -0300

    Add files via upload

--- test.py
+++ test.py
@@ -6,37 +6,46 @@ import pandas as pd
 ##################### FUNCTIONS #####################
 
 def search_google(browser, search_query):
-    search_url = f"https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&q={search_query}"
-    image_url = []
+    try:
+        search_url = f"https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&q={search_query}"
+        image_url = []
 
-    # open browser and begin search
-    browser.get(search_url)
-    print('before click')
-    element = browser.find_element_by_class_name('rg_i')
+        # open browser and begin search
+        browser.get(search_url)
+        # browser.implicitly_wait(1)
+        element = browser.find_element_by_class_name('rg_i')
 
-    # get images source url
-    element.click()
-    print('after click')
-    time.sleep(1)
+        # get images source url
+        element.click()
+        browser.implicitly_wait(1)
 
-    element = browser.find_elements_by_class_name('v4dQwb')        
-    big_img = element[0].find_element_by_class_name('n3VNCb')
-    image_url.append(big_img.get_attribute("src"))
+        element = browser.find_element_by_class_name('v4dQwb')
+        big_img = element.find_element_by_class_name('n3VNCb')
+        image_url.append(big_img.get_attribute("src"))
 
-    # download the image and write the name of the product so we can analise
-    # what's not adequate and change it manually
-    reponse = requests.get(image_url[0])
-    if reponse.status_code == 200:
-        with open(f"PROD " + search_query + ".jpg","wb") as file:
-            file.write(reponse.content)
+        # unless http it can't be downloaded so avoid them
+        if (image_url[0].startswith('http')):
 
-    link = ''.join(image_url[0])
-    return link
+            # download the image and write the name of the product so we can analise
+            # what's not adequate and change it manually
+            reponse = requests.get(image_url[0])
+            if reponse.status_code == 200:
+                with open(f"PROD " + search_query + ".jpg","wb") as file:
+                    file.write(reponse.content)
+                    return image_url
+            print('fuck')
+        else:
+            print(search_query + ' not downloaded')
+            return ['oops']
+    except:
+        print('not written, ' +search_query)
+        return ['']
 
 # 00. OPEN EXCEL AND WRITE IT TO A DATAFRAME
-excel = 'C:/Scraping/hmm.xls'
+excel = 'C:/Scraping/hm/result.xlsx'
 data = pd.read_excel(excel, 0)
 df = pd.DataFrame(data)
+
 
 # 01. COPY DATAFRAME DESCRIÇÃO SO THAT WE CAN PASS IT THROUGH 02 AND USE IT TO SEARCH
 selected_column = df[['Descrição']]
@@ -46,8 +55,8 @@ dfSearch = selected_column.copy()
 # if you include dots, it'll be messy
 dfSearch['Descrição'] = dfSearch['Descrição'].str.replace('.', ' ') 
 
-dfSearch['Descrição'] = dfSearch['Descrição'].str.replace('ANTITRASP.', 'ANTITRASPARENTE ')
-dfSearch['Descrição'] = dfSearch['Descrição'].str.replace('DESINF.', 'DESINFETANTE ')
+dfSearch['Descrição'] = dfSearch['Descrição'].str.replace('ANTITRASP ', 'ANTITRASPARENTE ')
+dfSearch['Descrição'] = dfSearch['Descrição'].str.replace('DESINF ', 'DESINFETANTE ')
 dfSearch['Descrição'] = dfSearch['Descrição'].str.replace('DESC ', 'DESCOLORANTE ')
 dfSearch['Descrição'] = dfSearch['Descrição'].str.replace('TONAL ', 'TONALIZANTE ')
 dfSearch['Descrição'] = dfSearch['Descrição'].str.replace('COND ', 'CONDICIONAR ')
@@ -82,8 +91,11 @@ dfSearch['Descrição'] = dfSearch['Descrição'].str.replace('Cr ', 'CREME ')
 
 # INITIALISING BROWSER HERE, OTHERWISE EVERY REQUEST WILL OPEN A NEW CMD AND BROWSER TAB
 browser = webdriver.Chrome('C:/Scraping/chromedriver')
+browser.set_window_position(0, 0)
+browser.set_window_size(500, 400)
 
-for numRow in range(1, 5):
+for numRow in range(1473, 2360):
+    # print(str(numRow) + ' starts')
     # READ DATAFRAME CONTENT AND SEND IT AS SEARCH_QUERY TO DEF SEARCHGOOGLE
     search_query_list = dfSearch.iat[numRow, 0]
     search_query = ''.join(search_query_list)
@@ -91,11 +103,12 @@ for numRow in range(1, 5):
     print(search_query)
     
     # CALL FOR FUNCTION, GRAB URL BY RETURNING ITS VALUE TO A STRING HERE
-    url = search_google(browser, search_query)
-    # url = ''.join(url_list)
+    url_list = search_google(browser, search_query)
+    url = ''.join(url_list)
 
     # APPLY CHANGES (irow, icolumn) OF ACTUAL DATAFRAME
-    df.iat[numRow, 7] = url
+    df.iat[numRow, len(df.columns)-1] = url
 
-# 04. WRITE TO EXCEL
-df.to_excel('prototype.xlsx', engine='xlsxwriter')
+    # 04. WRITE TO EXCEL
+    df.to_excel('result.xlsx', engine='xlsxwriter')
+    print(str(numRow) + ' ends\n')
